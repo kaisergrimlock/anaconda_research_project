acca47a1d284012695cacc1f825235629d22034f
kaisergrimlock <88642920+kaisergrimlock@users.noreply.github.com>
2026-03-27 14:00:43 +1100

    updated

--- scripts/report/seaborn_script/check_missing.py
+++ scripts/report/seaborn_script/check_missing.py
@@ -27,18 +27,19 @@ TREC_DL_YEAR = "2022"
 MODEL = "gpt-oss-20b"
 
 LANGUAGES = [
-    "ru_instruct_defended",
-    "zh_instruct_defended",
-    "ga_instruct_defended",
-    "ar_instruct_defended",
-    "fr_instruct_defended",
-    "vi_instruct_defended",
-    "sw_instruct_defended",
-    "ga_instruct_defended",
-    "eng_instruct_defended",
-    "hi_instruct_defended",
-    "he_instruct_defended",
-    "th_instruct_defended",
+    # "ru_instruct_defended",
+    # "zh_instruct_defended",
+    # "ga_instruct_defended",
+    # "ar_instruct_defended",
+    # "fr_instruct_defended",
+    # "vi_instruct_defended",
+    # "sw_instruct_defended",
+    # "ga_instruct_defended",
+    # "eng_instruct_defended",
+    # "hi_instruct_defended",
+    # "he_instruct_defended",
+    # "th_instruct_defended",
+    "eng_instruct" 
 ]
 
 
